823ca23cd1536c0489e93186697b94750d7c8933
Chris Towles <Chris.Towles@ge.com>
2022-10-17 23:55:13 -0400

    fix: add two easier tests

---
+++ exercises/typescript_problem_02_all_permutations/README.md
@@ -0,0 +1,25 @@
+# typescript_problem_all_permutations
+
+## requirements
+
+- Node 18+
+
+## Project setup
+
+```bash
+npm install
+```
+
+## Problem
+
+Find all permutations of an input string.
+
+## Constraints
+
+| Question                                          | Answer |
+| ------------------------------------------------- | ------ |
+| Can we assume the string is ASCII?                | Yes    |
+| Can the input have duplicates?                    | Yes    |
+| Can the output have duplicates?                   | No     |
+| Is the output a list of strings?                  | Yes    |
+| Do we have to output the results in sorted order? | No     |
---
+++ exercises/typescript_problem_02_all_permutations/src/main.spec.ts
@@ -0,0 +1,55 @@
+import { describe, it, expect } from 'vitest';
+
+/**
+ * Given to string, find all permintations.
+  *
+ * @param {string} value - first string
+ 
+ *
+ * @return {string[]} - all permutations
+ */
+function find_permutations(value: string): string[] {
+
+  // <-- ADD YOUR CODE HERE
+
+  
+
+
+  throw new Error("Function Not Implemented");
+}
+
+describe("Test", () => {
+
+  it("expect null if  find all permuntations", () => {
+    expect(find_permutations('')).toBe(['']);
+  });
+
+  it("expect to find all permuntations - small", () => {
+    
+    const testInput = 'ab'
+    const expected = ['ab', 'ba']
+    const result = find_permutations(testInput);
+    result.sort();
+    expected.sort();
+
+    expect(result).toBe(expected);
+
+
+  });
+  it("expect to find all permuntations", () => {
+    
+    const testInput = 'AABC'
+    const expected = [
+        'AABC', 'AACB', 'ABAC', 'ABCA',
+        'ACAB', 'ACBA', 'BAAC', 'BACA',
+        'BCAA', 'CAAB', 'CABA', 'CBAA'
+    ]
+    const result = find_permutations(testInput);
+    result.sort();
+    expected.sort();
+
+    expect(result).toBe(expected);
+
+
+  });
+});
